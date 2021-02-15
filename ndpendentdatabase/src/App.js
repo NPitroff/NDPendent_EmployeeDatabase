d7c541862dc17a83af46bfc330d9646fca6a1662
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-14 16:54:04 -0800

    Added the import for calling the API

--- ndpendentdatabase/src/App.js
+++ ndpendentdatabase/src/App.js
@@ -1,6 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Setting the Search state for employees
+
+class Search extends Component {
+  state = {
+    search: "",
+    employees: [],
+    filteredEmployees: [],
+    error: "",
+  };
+
+  // When the component mounts, get a list of all available base breeds and update this.state.breeds
+  componentDidMount() {
+    API.getEmployeeList(20, "US")
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
 function App() {
   return (
     <div className="App">

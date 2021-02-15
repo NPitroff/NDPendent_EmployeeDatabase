7fd35dceaa32757a2a43be8c8a1f5ee058393a95
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-14 16:46:41 -0800

    Added a Navbar file and export

---
+++ ndpendentdatabase/src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const navbar = () => {
+    return ( 
+        <nav className="nav">
+            <a className = "nav-link active" href="/">
+                NDPendent Coder Directory
+            </a>
+        </nav>
+    );
+};
+
+export default navbar;
